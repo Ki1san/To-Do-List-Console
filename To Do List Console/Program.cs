7c514de6916a43effa5947d9fc22e31e08fbe27d
Neki1 <soevkirill696@gmail.com>
2024-12-27 19:49:32 +0300

    Добавьте файлы проекта.

---
+++ To Do List Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class To_Do_List_Console
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("""
+            [1] Create a task
+            [2] Delete an issue
+            [3] View 
+            [4] File
+            [5] Exit
+            """);
+    }
+}
